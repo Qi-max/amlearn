f4ebfeb81b40d73e45a454110ca72eaedc92adb1
Qi-max <wqthu11@gmail.com>
2018-11-25 13:48:12 -0500

    add VoroIndex class

--- amlearn/featurize/featurizers/sro.py
+++ amlearn/featurize/featurizers/sro.py
@@ -64,3 +64,77 @@ class CNVoro(BaseFeaturize):
                or self.dependency_name == 'distance' \
             else ['CN_Voro']
         return feature_names
+
+
+class VoroIndex(BaseFeaturize):
+    def __init__(self, n_neighbor_limit, include_beyond_edge_max=False,
+                 atoms_df=None, dependency="voro", nn_kwargs=None,
+                 tmp_save=True, context=None):
+        """
+
+        Args:
+            dependency: (object or string) default: "voro"
+                if object, it can be "VoroNN()" or "DistanceNN()",
+                if string, it can be "voro" or "distance"
+        """
+        super(self.__class__, self).__init__(tmp_save=tmp_save,
+                                             context=context,
+                                             dependency=dependency,
+                                             nn_kwargs=nn_kwargs,
+                                             atoms_df=atoms_df)
+        self.n_neighbor_limit = n_neighbor_limit
+        self.include_beyond_edge_max = include_beyond_edge_max
+
+    def fit(self, X=None):
+        X = check_featurizer_X(X=X, atoms_df=self.atoms_df)
+        columns = X.columns
+
+        if self._dependency.__class__.__name__ == "VoroNN" and \
+                        'n_neighbors_voro' in columns:
+            return self
+        elif self._dependency.__class__.__name__ == "DistanceNN" and \
+                        'n_neighbors_dist' in columns:
+            return self
+        else:
+            self.atoms_df = self._dependency.fit_transform(X)
+            return self
+
+    def transform(self, X=None):
+        X = check_featurizer_X(X=X, atoms_df=self.atoms_df)
+        n_atoms = len(X)
+        columns = X.columns
+        edge_cols = [col for col in columns if col.startswith('neighbor_edge_')]
+        edge_list = [int(col.split('_')[-1]) for col in edge_cols]
+        edge_num = len(edge_list)
+        self.edge_min = min(edge_list)
+        self.edge_max = max(edge_list)
+
+        voronoi_index_list = np.zeros((n_atoms, edge_num))
+
+        print(voronoi_stats.voronoi_index.__doc__)
+        voro_index_list = \
+            voronoi_stats.voronoi_index(voronoi_index_list,
+                                        X['n_neighbors_voro'].values(),
+                                        X[edge_cols].values(),
+                                        self.edge_min, self.edge_max,
+                                        self.include_beyond_edge_max,
+                                        n_atoms=n_atoms,
+                                        n_neighbor_limit=self.n_neighbor_limit)
+
+        voro_index_df = pd.DataFrame(voro_index_list,
+                                     index=range(n_atoms),
+                                     columns=self.get_feature_names())
+
+        if self.tmp_save:
+            self.context.save_featurizer_as_dataframe(output_df=voro_index_df,
+                                                      name='cn_voro')
+
+        return voro_index_df
+
+    def get_feature_names(self):
+        feature_names = ['Voronoi idx_{}'.format(edge)
+                         for edge in range(self.edge_min,
+                                           self.edge_max + 1)]
+        return feature_names
+
+
