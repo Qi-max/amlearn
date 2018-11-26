4a655c0022360d31bdf1f1b796c867732870dc75
Qi Wang <wqthu11@gmail.com>
2018-11-26 00:04:09 -0500

    add CharacterMotif class which based in VoroIndex

--- amlearn/featurize/featurizers/sro.py
+++ amlearn/featurize/featurizers/sro.py
@@ -107,8 +107,8 @@ class CN(BaseSro):
 
 class VoroIndex(BaseSro):
     def __init__(self, n_neighbor_limit, include_beyond_edge_max=False,
-                 atoms_df=None, dependency="voro", nn_kwargs=None,
-                 tmp_save=True, context=None):
+                 atoms_df=None, dependency="voro",
+                 tmp_save=True, context=None, **nn_kwargs):
         """
 
         Args:
@@ -119,14 +119,14 @@ class VoroIndex(BaseSro):
         super(VoroIndex, self).__init__(tmp_save=tmp_save,
                                         context=context,
                                         dependency=dependency,
-                                        nn_kwargs=nn_kwargs,
-                                        atoms_df=atoms_df)
+                                        atoms_df=atoms_df,
+                                        **nn_kwargs)
         self.n_neighbor_limit = n_neighbor_limit
         self.include_beyond_edge_max = include_beyond_edge_max
 
     def fit(self, X=None):
         X = check_featurizer_X(X=X, atoms_df=self.atoms_df)
-        voro_depend_cols = ['n_neighbors_voro', 'neighbor_edge_0']
+        voro_depend_cols = ['n_neighbors_voro', 'neighbor_edge_5']
         if self._dependency is None:
             return self
         elif check_dependency(depend_cls=self._dependency,
@@ -176,4 +176,89 @@ class VoroIndex(BaseSro):
                                            self.edge_max + 1)]
         return feature_names
 
+
+class CharacterMotif(BaseSro):
+    def __init__(self, n_neighbor_limit, include_beyond_edge_max=False,
+                 atoms_df=None, target_voro_idx=None, frank_kasper=1,
+                 dependency="voro", tmp_save=True, context=None,
+                 **nn_kwargs):
+        super(CharacterMotif, self).__init__(tmp_save=tmp_save,
+                                             context=context,
+                                             dependency=dependency,
+                                             atoms_df=atoms_df,
+                                             **nn_kwargs)
+        self.n_neighbor_limit = n_neighbor_limit
+        self.include_beyond_edge_max = include_beyond_edge_max
+        if target_voro_idx is None:
+            self.target_voro_idx = np.array([[0, 0, 12, 0, 0],
+                                             [0, 0, 12, 4, 0]],
+                                            dtype=np.float128)
+        self.frank_kasper = frank_kasper
+
+    def fit(self, X=None):
+        X = check_featurizer_X(X=X, atoms_df=self.atoms_df)
+        voro_depend_cols = ['n_neighbors_voro', 'neighbor_edge_5']
+        if self._dependency is None:
+            return self
+        elif check_dependency(depend_cls=self._dependency,
+                              df_cols=X.columns,
+                              voro_depend_cols=voro_depend_cols,
+                              dist_denpend_cols=None):
+            return self
+        elif check_dependency(depend_cls=self._dependency,
+                              df_cols=X.columns,
+                              voro_depend_cols=['Voronoi idx_5'],
+                              dist_denpend_cols=None):
+            return self
+        else:
+            self.atoms_df = self._dependency.fit_transform(X)
+            return self
+
+    def transform(self, X=None):
+        X = check_featurizer_X(X=X, atoms_df=self.atoms_df)
+        n_atoms = len(X)
+        columns = X.columns
+        if "Voronoi idx_5" not in columns:
+            voro_index = \
+                VoroIndex(n_neighbor_limit=self.n_neighbor_limit,
+                          include_beyond_edge_max=self.include_beyond_edge_max,
+                          atoms_df=X, dependency=self.dependency,
+                          tmp_save=False, context=self.context)
+            X = voro_index.fit_transform(X)
+
+        voro_index_cols = [col for col in columns
+                           if col.startswith("Voronoi idx_")]
+        edge_min = min([int(col.split("_")[-1]) for col in voro_index_cols])
+
+        motif_one_hot = np.zeros((n_atoms,
+                                  len(self.target_voro_idx) + self.frank_kasper))
+
+        motif_one_hot = \
+            voronoi_stats.character_motif(motif_one_hot,
+                                          X[voro_index_cols].values,
+                                          edge_min, self.target_voro_idx,
+                                          self.frank_kasper,
+                                          n_atoms=n_atoms)
+        motif_one_hot_array = np.array(motif_one_hot)
+        is_120_124 = motif_one_hot_array[:, 0] | motif_one_hot_array[:, 1]
+        print(motif_one_hot_array.shape)
+        print(is_120_124.shape)
+        motif_one_hot_array = np.append(motif_one_hot_array,
+                                        np.array([is_120_124]).T, axis=1)
+        character_motif_df = pd.DataFrame(motif_one_hot_array,
+                                          index=range(n_atoms),
+                                          columns=self.get_feature_names())
+
+        if self.tmp_save:
+            self.context.save_featurizer_as_dataframe(
+                output_df=character_motif_df, name='character_motif')
+
+        return character_motif_df
+
+    def get_feature_names(self):
+        feature_names = ['is <0,0,12,0,0>', 'is <0,0,12,4,0>'] + \
+                        ["_".join(map(str, v))
+                         for v in self.target_voro_idx[2:]] + \
+                        ['is polytetrahedral', 'is <0,0,12,0/4,0>']
+        return feature_names
 
