b3b90a69f045a1566adcbfab170780ad57efac76
Qi-max <wqthu11@gmail.com>
2019-04-16 15:12:42 -0700

    add best_model,feature_importances_,save_model and predict functions

--- amlearn/learn/classifier.py
+++ amlearn/learn/classifier.py
@@ -8,15 +8,19 @@ to evaluate estimator's performance.
 import os
 import numpy as np
 from copy import copy
-from amlearn.learn.base_learn import AmBaseLearn
+
+from amlearn.learn.base import AmBaseLearn
 from amlearn.learn.sklearn_patch import cross_validate
 from amlearn.learn.sklearn_patch import calc_scores
 from amlearn.preprocess.imblearn_preprocess import ImblearnPreprocess
+from amlearn.utils.check import check_output_path
 from amlearn.utils.data import list_like
 from amlearn.utils.directory import write_file
 from sklearn.base import ClassifierMixin
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.utils.testing import all_estimators
+from sklearn.utils.validation import check_is_fitted
 
 
 class AmClassifier(AmBaseLearn):
@@ -30,45 +34,53 @@ class AmClassifier(AmBaseLearn):
         seed:
 
     """
-    def __init__(self, backend, classifier=None, imblearn=False,
+    def __init__(self, backend, classifier=None, classifier_params=None,
                  decimals=None, seed=1):
         classifiers_dict = dict(self.valid_components)
         if classifier is None:
-            classifier = self.best_classifier
-        if isinstance(classifier, type):
-            classifier = classifier.__name__
-        if classifier not in classifiers_dict.keys():
+            classifier_name = self.default_classifier
+        elif isinstance(classifier, ClassifierMixin):
+            classifier_name = classifier.__class__.__name__
+        elif isinstance(classifier, type):
+            classifier_name = classifier.__name__
+        elif isinstance(classifier, str):
+            classifier_name = classifier
+        else:
+            classifier_name = None
+
+        if classifier_name not in classifiers_dict.keys():
             raise ValueError('Classifier {} is unknown, Possible values '
                              'are {}'.format(classifier,
                                              classifiers_dict.keys()))
-        self.classifier = classifiers_dict[classifier]
+        if classifier_params is None:
+            classifier_params = {}
+        self.classifier = classifier if isinstance(classifier, ClassifierMixin) \
+            else classifiers_dict[classifier_name](**classifier_params)
         super().__init__(backend, decimals=decimals, seed=seed)
-        self.imblearn = imblearn
 
     def fit(self, X, y, random_state=None,
-            cv_num=1, cv_kwargs=None, test_size=0.3,
-            imblearn=False, imblearn_method=None, imblearn_kwargs=None,
+            cv_num=1, cv_params=None, test_size=0.3,
+            imblearn=False, imblearn_method=None, imblearn_params=None,
             save_model=True, save_predict=True, save_train_test_idx=True,
-            predict_types='dataframe', **classifier_kwargs):
+            predict_types='dataframe'):
 
         if imblearn:
             self._fit_imblearn(
                 X, y, random_state=random_state,
-                cv_num=cv_num, cv_kwargs=cv_kwargs, test_size=test_size,
+                cv_num=cv_num, cv_params=cv_params, test_size=test_size,
                 imblearn_method=imblearn_method,
-                imblearn_kwargs=imblearn_kwargs, save_model=save_model,
+                imblearn_params=imblearn_params, save_model=save_model,
                 save_predict=save_predict,
                 save_train_test_idx=save_train_test_idx,
-                predict_types=predict_types, **classifier_kwargs)
+                predict_types=predict_types)
 
         else:
             self._fit_cv(
                 X, y, random_state=random_state,
-                cv_kwargs=cv_kwargs, cv_num=cv_num, test_size=test_size,
+                cv_params=cv_params, cv_num=cv_num, test_size=test_size,
                 save_model=save_model, save_predict=save_predict,
                 predict_types=predict_types,
-                save_train_test_idx=save_train_test_idx,
-                **classifier_kwargs)
+                save_train_test_idx=save_train_test_idx)
 
     def _fit(self, X, y, classifier, test_size=0.3,
              random_state=None, scoring=None, return_train_score=False):
@@ -109,89 +121,94 @@ class AmClassifier(AmBaseLearn):
         return ret
 
     def _fit_cv(self, X, y, random_state=None,
-                cv_num=1, cv_kwargs=None, test_size=0.3,
+                cv_num=1, cv_params=None, test_size=0.3,
                 save_model=True, save_predict=True, predict_types='dataframe',
-                save_train_test_idx=True, **classifier_kwargs):
+                save_train_test_idx=True):
 
-        if hasattr(self.classifier, 'random_state'):
-            classifier = self.classifier(random_state=random_state,
-                                         **classifier_kwargs)
-        else:
-            classifier = self.classifier(**classifier_kwargs)
+        # If user's cv_params contains 'cv_num' parameter, use the max value
+        # between function parameter 'cv_num' and cv_params's 'cv_num'.
+        if cv_params is None:
+            cv_params = {}
 
-        # If user's cv_kwargs contains 'cv_num' parameter, use the max value
-        # between function parameter 'cv_num' and cv_kwargs's 'cv_num'.
-        if 'cv_num' in cv_kwargs.keys():
-            cv_num = max(cv_num, cv_kwargs['cv_num'])
-            cv_kwargs.pop('cv_num')
+        if 'cv_num' in cv_params.keys():
+            cv_num = max(cv_num, cv_params['cv_num'])
+            cv_params.pop('cv_num')
 
         if cv_num > 1:
             np.random.seed(random_state)
-            scores = cross_validate(estimator=classifier, X=X, y=y, cv=cv_num,
-                                    **cv_kwargs)
+            scores = cross_validate(estimator=self.classifier,
+                                    X=X, y=y, cv=cv_num, **cv_params)
 
         else:
             scores = self._fit(
-                X, y, classifier,
-                test_size=test_size,
-                random_state=random_state,
-                return_train_score=cv_kwargs.get('return_train_score', False),
-                scoring=cv_kwargs.get('scoring', None))
+                X, y, self.classifier,
+                test_size=test_size, random_state=random_state,
+                return_train_score=cv_params.get('return_train_score', False),
+                scoring=cv_params.get('scoring', None))
 
             cv_num = 1
+
+        self.best_score_, (self.best_model_, self.best_model_tag_)= \
+            max(zip(scores['test_score'],
+                    zip(scores['models'],
+                        [''] if cv_num == 1 else
+                        ["cv_{}".format(i) for i in range(cv_num)])),
+                key=lambda x: x[0])
 
         # get temporary path, if self._tmp_path exist get it (most create by
         # imblearn), else get self.backend.tmp_path
         tmp_path = self._tmp_path \
             if hasattr(self, '_tmp_path') else self.backend.tmp_path
 
-        for cv_idx in range(cv_num):
-            self.backend.tmp_path = os.path.join(tmp_path,
-                                                 "cv_{}".format(cv_idx))
-            score_model = scores['models'][cv_idx]
-            if save_model:
-                self.backend.save_model(score_model, self.seed)
-            if save_train_test_idx:
-                train_idx = scores['indexs'][cv_idx][0]
-                test_idx = scores['indexs'][cv_idx][1]
-                write_file(
-                    os.path.join(self.backend.tmp_path,
-                                 'train_idx.txt'),
-                    "\n".join(list(map(str, train_idx))))
+        if save_model or save_train_test_idx or save_predict:
+            for cv_idx in range(cv_num):
+                print(cv_idx)
+                print(tmp_path)
+                print("cv_{}".format(cv_idx))
+                tmp_path_cv = os.path.join(tmp_path, "cv_{}".format(cv_idx))
+                check_output_path(tmp_path_cv)
+                score_model = scores['models'][cv_idx]
+                if save_model:
+                    self.backend.save_model(score_model, self.seed)
+                if save_train_test_idx:
+                    train_idx = scores['indexs'][cv_idx][0]
+                    test_idx = scores['indexs'][cv_idx][1]
+                    write_file( os.path.join(tmp_path_cv, 'train_idx.txt'),
+                                "\n".join(list(map(str, train_idx))))
 
-                write_file(
-                    os.path.join(self.backend.tmp_path,
-                                 'test_idx.txt'),
-                    "\n".join(list(map(str, test_idx))))
+                    write_file( os.path.join(tmp_path_cv, 'test_idx.txt'),
+                                "\n".join(list(map(str, test_idx))))
 
-            if save_predict:
-                predictions = score_model.predict(X)
-                if not isinstance(predict_types, list_like):
-                    predict_types = [predict_types]
-                for predict_type in predict_types:
-                    if predict_type in self.backend.valid_predictions_type:
-                        self.backend.eval('save_predictions_as_{}'.format(
-                            predict_type))(predictions, self.seed)
-                    else:
-                        raise ValueError(
-                            'predict_type {} is unknown, '
-                            'Possible values are {}'.format(
-                                predict_type,
-                                self.backend.valid_predictions_type))
+                if save_predict:
+                    predictions = score_model.predict(X)
+                    if not isinstance(predict_types, list_like()):
+                        predict_types = [predict_types]
+                    for predict_type in predict_types:
+                        if predict_type in self.backend.valid_predictions_type:
+                            getattr(self.backend,
+                                    'save_predictions_as_{}'.format(
+                                        predict_type))(predictions, self.seed)
+                        else:
+                            raise ValueError(
+                                'predict_type {} is unknown, '
+                                'Possible values are {}'.format(
+                                    predict_type,
+                                    self.backend.valid_predictions_type))
         self.backend.tmp_persistence(tmp_path)
         return self
 
     def _fit_imblearn(self, X, y, random_state=None,
-                      imblearn_method=None, imblearn_kwargs=None,
-                      cv_num=1, cv_kwargs=None, test_size=0.3,
+                      imblearn_method=None, imblearn_params=None,
+                      cv_num=1, cv_params=None, test_size=0.3,
                       save_model=True, save_predict=True,
                       save_train_test_idx=True,
-                      predict_types='dataframe', **classifier_kwargs):
+                      predict_types='dataframe', classifier_params=None):
         imblearn = ImblearnPreprocess()
-        if 'random_state' not in imblearn_kwargs:
-            imblearn_kwargs['random_state'] = random_state
-        X, y = imblearn.fit(X, y, imblearn_method, imblearn_kwargs)
+        if 'random_state' not in imblearn_params:
+            imblearn_params['random_state'] = random_state
+        X, y = imblearn.fit(X, y, imblearn_method, imblearn_params)
 
+        score_model_list = list()
         # get the imblearn n_subsets num from X shape.
         if len(X.shape) == 2:
             n_subsets = 1
@@ -209,31 +226,66 @@ class AmClassifier(AmBaseLearn):
                                           'imblearn_{}'.format(imblearn_idx))
             self._fit_cv(
                 X=X_imb, y=y_imb, random_state=random_state,
-                cv_kwargs=cv_kwargs, cv_num=cv_num, test_size=test_size,
+                cv_params=cv_params, cv_num=cv_num, test_size=test_size,
                 save_model=save_model, save_predict=save_predict,
                 predict_types=predict_types,
-                save_train_test_idx=save_train_test_idx,
-                **classifier_kwargs)
+                save_train_test_idx=save_train_test_idx)
+            score_model_list.append(
+                (self.best_score_,
+                 (self.best_model_,
+                  "imblearn_{}_{}".format(imblearn_idx, self.best_model_tag_))))
+        print(score_model_list)
+        self.best_score_, (self.best_model_, self.best_model_tag_) = \
+            max(score_model_list, key=lambda x: x[0])
         return self
 
-    def predict(self, X, y):
+    def predict(self, X):
+        check_is_fitted(self, 'best_model_')
+        return self.best_model_.predict(X)
 
-        return
+    def score(self, X):
+        check_is_fitted(self, 'best_model_')
+        pass
+
+    def predict_proba(self, X):
+        check_is_fitted(self, 'best_model_')
+        return self.best_model_.predict_proba(X)
+
+    def predict_log_proba(self, X):
+        check_is_fitted(self, 'best_model_')
+        return self.best_model_.predict_log_proba(X)
+
+    def save_best_model(self):
+        check_is_fitted(self, 'best_model_')
+        model_file = \
+            os.path.join(self.backend.output_path,
+                         'best_model_{}.pkl'.format(self.best_model_tag_))
+        joblib.dump(self.best_model_, model_file)
 
     @property
-    def best_classifier(self):
-        """Read the default environment file to find the best comprehensive
+    def best_model(self):
+        check_is_fitted(self, 'best_model_')
+        return self.best_model_
+
+    @property
+    def feature_importances_(self):
+        check_is_fitted(self, 'best_model_')
+        return self.best_model_.feature_importances_
+
+    @property
+    def default_classifier(self):
+        """Read the default environment file to find the default comprehensive
            classifier at present.
 
 
         Returns:
 
-        _best_classifier: object
-            Returns best classifier
+        _default_classifier: object
+            Returns default classifier
 
         """
-        self._best_classifier = self.backend.def_env["best_classifier"]
-        return self._best_classifier
+        self.default_classifier_ = self.backend.def_env["default_classifier"]
+        return self.default_classifier_
 
     @property
     def valid_components(self):
@@ -242,9 +294,9 @@ class AmClassifier(AmBaseLearn):
         valid_components: numpy.array([[classifier name, object], ...])
                           Returns valid classifiers
         """
-        if not hasattr(self, "_valid_components"):
+        if not hasattr(self, "valid_components_"):
             classifiers = np.array([est for est in all_estimators() if
                                     issubclass(est[1], ClassifierMixin)])
 
-            self._valid_components = classifiers
-        return self._valid_components
+            self.valid_components_ = classifiers
+        return self.valid_components_
